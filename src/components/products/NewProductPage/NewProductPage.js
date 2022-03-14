0b1c2bf6af4184157a14f2321e8cbedc134ad990
Sheyla Perez <sheylapdv@gmail.com>
2022-03-14 18:16:02 +0100

    NewProductsPage

---
+++ src/components/products/NewProductPage/NewProductPage.js
@@ -0,0 +1,11 @@
+import Layout from "../../layout/layout.js";
+
+const NewProductPage = () => {
+  return (
+    <Layout title="Este es tu producto">
+      <div>Página de producto</div>
+    </Layout>
+  );
+};
+
+export default NewProductPage;
