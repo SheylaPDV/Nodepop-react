bcef2783ae053af42a56b497082ed9e4979daaf4
Sheyla Perez <sheylapdv@gmail.com>
2022-03-14 18:16:24 +0100

    cambios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import ProductsPage from "./components/products/ProductsPage.js";
-import Button from "./components/products/common/button.js";
+import Button from "./components/common/button.js";
+import NewProductPage from "./components/products/NewProductPage/NewProductPage.js";
+import { useState } from 'react';
+import classNames from "classnames";
+import LoginPage from "./components/auth/loginPage/LoginPage.js";
+
+
 
 function App() {
+  const container = true;
   return (
-    <div className="App">
-      <ProductsPage/>
-      <Button variant="primary">Primary</Button>
-      <Button>Normal</Button>
-      <Button disabled>Normal</Button>
+    <div className={classNames("App", { container })}>
+      {/* <ProductsPage />
+      <NewProductPage />
+      <ProductsPage />
+       */}
+       <LoginPage></LoginPage>
     </div>
   );
 }
