393099711d5d1bd5ed66f79f8705bd4f19bde180
Sheyla Perez <sheylapdv@gmail.com>
2022-03-27 12:46:10 +0200

    creando boton borrar

--- src/components/products/productsPage/Product.js
+++ src/components/products/productsPage/Product.js
@@ -2,30 +2,54 @@ import React from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import Photo from '../../common/Photo';
 import '../../../assets/css/Product.css';
+import Button from '../../common/button';
+import { useState, setError } from 'react';
+import { deleteProduct } from '../../service';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 //////////////////////////PRODUCTO///////////////////////////////
 
-const Product = ({ content, updatedAt, user }) => {
+const Product = ({ id, name, sale, price, tags }) => {
+  const navigate = useNavigate();
+  const [product, setProduct] = useState({
+    id: '',
+    name: '',
+    price: 0,
+    sale: false,
+    tags: [],
+  });
+  const [deletedProduct, setDeletedProduct] = useState(null);
+
+  const handleClick = async (id) => {
+    try {
+      console.log(id);
+      const borrar = await deleteProduct(id);
+      setDeletedProduct(borrar);
+      console.log(borrar);
+      navigate(`/products`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (deletedProduct) {
+    return <Navigate to={`/products`} />;
+  }
+
   return (
     <article className="product bordered">
-      <div className="left">
-        <Photo className="product-photo" />
-      </div>
+      <div className="left">{/* <Photo className="product-photo" /> */}</div>
       <div className="right">
         <div className="product-header">
-          <span className="product-name">{user.name}</span>
-          <span className="product-username">{user.username}</span>
+          <span className="product-name">Nombre: {name}</span>
+          <span className="product-username">{sale}</span>
+          <span className="product-name">Precio: {price}</span>
+          <span className="product-name">{tags}</span>
           <span className="product-separator">·</span>
-          <time dateTime={updatedAt}>
-            {formatDistanceToNow(new Date(updatedAt))}
-          </time>
         </div>
         <div>
-          {content}
           <div className="product-actions">
-            {/* <LikeButton onLike={event => console.log(event)}>
-              {likes.length || null}
-            </LikeButton> */}
+            <button onClick={() => handleClick(id)}>Borrar</button>
           </div>
         </div>
       </div>
